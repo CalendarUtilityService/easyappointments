80a043f0729fb229a23c56c93d483a17aa9fd5d8
L13w <github@in.llew.net>
2023-03-22 10:53:04 -0700

    Initial commit

---
+++ storage/logs/log-2023-03-14.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-03-14 23:47:52 --> Could not find the language line "Array appointment_registered"
+ERROR - 2023-03-14 23:49:51 --> Could not find the language line " appointment_registered"
+ERROR - 2023-03-14 23:49:51 --> Could not find the language line "Array appointment_details_was_sent_to_you"
---
+++ storage/logs/log-2023-03-15.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-03-15 00:01:30 --> Could not find the language line " appointment_registered"
+ERROR - 2023-03-15 00:01:30 --> Could not find the language line "Array appointment_details_was_sent_to_you"
+ERROR - 2023-03-15 16:20:22 --> Could not find the language line " appointment_registered"
+ERROR - 2023-03-15 16:20:22 --> Could not find the language line "Array appointment_details_was_sent_to_you"
